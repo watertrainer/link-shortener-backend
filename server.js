5795ec8b0920df0ae31253d8a1c3f1897c00b69e
watertrainer <watertrainer42@gmail.com>
2022-08-20 18:09:51 +0000

    server ignore host to be visible through docker

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const handler = require("./requestHandler");
 const { Pool } = require('pg')
 const pool = new Pool()
-const hostname = '127.0.0.1';
 const port = 3000;
 
 
@@ -11,6 +10,6 @@ const server = http.createServer(async (req, res) => {
     await handler.handleRequest(req, res, pool);
 });
 
-server.listen(port, hostname, async () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port,async () => {
+    console.log(`Server running at ${port}`);
 });
